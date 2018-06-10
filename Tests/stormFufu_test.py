c759a095a0aff97bce9c45014a36ebf19fa8cef7
dimagic <dimag.dn@gmail.com>
2018-06-10 23:42:20 +0300

    0.3.17

--- Tests/stormFufu_test.py
+++ Tests/stormFufu_test.py
@@ -1,4 +1,6 @@
 import time
+import re
+import os
 from PyQt5 import QtCore
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,17 +17,10 @@ class StormFufuTest(QtCore.QThread):
         self.driver = webdriver.Ie(executable_path=r"Drivers/IEDriverServer.exe")
         self.driver.implicitly_wait(3)
 
+        self.reName = re.compile(r"&nbsp;[\w+]</TD>")
+
     def run(self):
-        # self.msgSignal.emit('i', 'RFBChech', 'FUFU', 1)
-        # try:
-        #     driver = webdriver.Chrome(executable_path=r"Drivers/chromedriver.exe")
-        # except Exception as e:
-        #     print(str(e))
-        # else:
-
-
         self.driver.get("http://192.168.1.253")
-        # assert "Python" in driver.title
         elem = self.driver.find_element_by_id('user')
         elem.clear()
         elem.send_keys("deko")
@@ -33,18 +28,6 @@ class StormFufuTest(QtCore.QThread):
         elem.clear()
         elem.send_keys("deko10")
         elem.send_keys(Keys.RETURN)
-        # WebDriverWait(driver, timeout).until(elem)
-
-        # for i in ('Alarms', 'Control&amp;Params'):
-        #     try:
-        #         driver.find_element_by_link_text(i).click()
-        #     except Exception:
-        #         print('Element: ' + i + ' not found')
-
-        # links = driver.find_elements_by_partial_link_text('##')
-        # for link in links:
-        #     print(link.get_attribute("href"))
-
         pages = ('Alarms', 'Control&Params', 'Band Info', 'SW Upgrade', 'License')
         for i in pages:
             elem = self.driver.find_element_by_xpath("//a[text()='" + i + "']")
@@ -53,22 +36,16 @@ class StormFufuTest(QtCore.QThread):
             time.sleep(2)
             if i == pages[0]:
                 self.pageAlarms()
-        # ActionChains(driver).move_to_element(elem).click()
-        # elem.click()
-
-            # WebDriverWait(driver, timeout).until(elem)
-
-        # elems = driver.find_elements_by_class_name("tab")
-        # for elem in elems:
-        #     print(elem.get_attribute("href"))
-
-            # if 'alarms' in elem.get_attribute("href"):
-            #     elem.send_keys(Keys.RETURN)
-
-        # assert "No results found." not in driver.page_source
-        time.sleep(10)
         self.driver.close()
+        os.system("taskkill /f /im IEDriverServer.exe")
 
     def pageAlarms(self):
-        for elem in self.driver.find_elements_by_class_name("NeAlarmTblImg"):
+        for elem in self.driver.find_elements_by_xpath(".//tr/td[@class='NeAlarmTbl']"):
             print(elem.get_attribute('innerHTML'))
+            for alarm in elem.find_elements_by_xpath("../td/div"):
+                print(alarm.get_attribute('innerHTML'))
+            print("------------------------------------------------------------------------")
+
+
+
+
